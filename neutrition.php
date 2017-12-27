d53191333954d8515953125f8b381fdb5724c0bc
BilllYang <a990200@gmail.com>
2017-12-27 18:00:46 +0800

    fix some bugs

--- neutrition.php
+++ neutrition.php
@@ -2,58 +2,58 @@
     <!DOCTYPE html>
 <html>
 <body background="map.jpg">
-<form action="/project/search_1117_ui.php" method="POST">
-cost&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text"><select name="cost">
+<form action="/project/search_1227_ui.php" method="POST">
+cost&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name = "Ycost"><select name="cost">
 　<option value="0">忽略</option>
 　<option value="1">至少</option>
 　<option value="2">至多</option>
 </select><br><br>
-carlories&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text"><select name="carlories">
+carlories&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="Ycarlories"><select name="carlories">
 　<option value="0">忽略</option>
 　<option value="1">至少</option>
 　<option value="2">至多</option>
 </select><br><br>
-proteins&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text"><select name="proteins">
+proteins&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="Yproteins"><select name="proteins">
 　<option value="0">忽略</option>
 　<option value="1">至少</option>
 　<option value="2">至多</option>
 </select><br><br>
-carbohydrates:<input type="text"><select name="carbohydrates">
+carbohydrates:<input type="text" name="Ycarbohydrates"><select name="carbohydrates">
 　<option value="0">忽略</option>
 　<option value="1">至少</option>
 　<option value="2">至多</option>
 </select><br><br>
-vitamin A&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text"><select name="vitaminA">
+vitamin A&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="YvitaminA"><select name="vitaminA">
 　<option value="0">忽略</option>
 　<option value="1">至少</option>
 　<option value="2">至多</option>
 </select><br><br> 
-calcium &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text"><select name="calcium">
+calcium &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="Ycalcium"><select name="calcium">
 　<option value="0">忽略</option>
 　<option value="1">至少</option>
 　<option value="2">至多</option>
 </select><br><br>
-fat&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text"><select name="fat">
+fat&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="Yfat"><select name="fat">
 　<option value="0">忽略</option>
 　<option value="1">至少</option>
 　<option value="2">至多</option>
 </select><br><br>
-cholesterol&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text"><select name="cholesterol">
+cholesterol&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="Ycholesterol"><select name="cholesterol">
 　<option value="0">忽略</option>
 　<option value="1">至少</option>
 　<option value="2">至多</option>
 </select><br><br> 
-sodium&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text"><select name="sodium">
+sodium&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="Ysodium"><select name="sodium">
 　<option value="0">忽略</option>
 　<option value="1">至少</option>
 　<option value="2">至多</option>
 </select><br><br>
-fiber&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text"><select name="fiber">
+fiber&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="Yfiber"><select name="fiber">
 　<option value="0">忽略</option>
 　<option value="1">至少</option>
 　<option value="2">至多</option>
 </select><br><br>
-vitamin C&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text"><select name="vitaminC">
+vitamin C&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="YvitaminC"><select name="vitaminC">
 　<option value="0">忽略</option>
 　<option value="1">至少</option>
 　<option value="2">至多</option>
@@ -61,28 +61,29 @@ vitamin C&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text"><sel
 <br>
 <br>
 <br>
-objective: <select name="Yourcost">
+objective: <select name="objective">
 　<option value="0">cost</option>
 　<option value="1">carlories</option>
 　<option value="2">carbohydrate</option>
   <option value="3">protein/(fat+1)</option>
   <option value="4">protein</option>
 </select><br>
-<input type="submit" value="Submit" onclick="to_search()">
+<input type="submit" value="Submit">
 </form>
 <?php
   $a = $_POST['a'];
   $b = $_POST['b'];
   $c = $_POST['c'];
+  $txt = ".txt";
+  $neutrition = "neutrition.txt";
+  $neutrition = $a.$neutrition;
+  $a = $a.$txt;
 
-  $myfile = fopen("result.txt", "w"); 
-  fwrite($myfile,$a);
-  fwrite($myfile,"|");
+  $myfile = fopen($a, "w"); 
   fwrite($myfile,$b);
-  fwrite($myfile,"|");
+  fwrite($myfile,",");
   fwrite($myfile,$c);
   fclose($myfile);
-  $file_content = file_get_contents("result.txt");
   $answer = shell_exec("a.exe a b");
 ?>
 <script type="text/javascript">
