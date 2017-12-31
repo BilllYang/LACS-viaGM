db55ac4f2e285135d62ed7683a872721e0f18478
BilllYang <a990200@gmail.com>
2018-01-01 06:30:46 +0800

    happy new year
    fix solve2 bug
    change nutrition ui 
    add foolproof in search

--- solve2.php
+++ solve2.php
@@ -3,6 +3,6 @@
   $new = "/opt/gurobi752/linux64/lib";
   $new .= ":$path";
   putenv("LD_LIBRARY_PATH=$new");
-  $answer = shell_exec("./solveDiet table.txt");
+  $answer = shell_exec("./solveDiet out");
   header("Location: /project/search_1227_ui.php");
 ?>
